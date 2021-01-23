6b457679e624201ffdc98803e16a6153d9db8ec6
Yuvaraja Kesavan <yuvar4u@gmail.com>
2021-01-23 18:39:39 +0530

    third commit

--- LockedMe/src/com/project/lockedme/IterateFilesFolders.java
+++ LockedMe/src/com/project/lockedme/IterateFilesFolders.java
@@ -2,6 +2,7 @@ package com.project.lockedme;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
@@ -11,27 +12,36 @@ public class IterateFilesFolders {
 	@SuppressWarnings("unchecked")
 	public void display() {
 		try {
-			List listFile = new ArrayList();
+			ArrayList listFile = new ArrayList();
+			ArrayList listFolder = new ArrayList();
 			String folderPath = MainClass.folderLocation;
 			Scanner scan = new Scanner(folderPath);
 			File folder = new File(folderPath);
 			File[] files = folder.listFiles();
-
+			Arrays.sort(files);
+			
+			System.out.println(Arrays.toString(files));
 			for (File file : files) {
 				if (file.isFile()) {
-					System.out.println("File Name: " + file.getName());
+					//System.out.println("File Name: " + file.getName());
 					listFile.add(file.getName());
 				} else if (file.isDirectory()) {
-					System.out.println("\nFolder Name: " + file.getName());
+					//System.out.println("\nFolder Name: " + file.getName());
+					listFolder.add(file.getName());
 				}
-
 			}
-			System.out.println("listFile ==> " +listFile);
 			Collections.sort(listFile);
-			Iterator it = listFile.iterator();
-			if(it.hasNext()) {
-				System.out.println("File Name: " + it.next());
-			}
+			Collections.sort(listFolder);
+			Iterator itr = listFile.iterator();
+			System.out.println("\nFiles list in Ascending Order : ");
+			while (itr.hasNext()) { 
+	            System.out.println(itr.next()); 
+	        }
+			System.out.println("\nFolder list in Ascending Order : ");
+			Iterator itrFolder = listFolder.iterator();
+			while (itrFolder.hasNext()) { 
+	            System.out.println(itrFolder.next()); 
+	        }
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -62,7 +72,6 @@ public class IterateFilesFolders {
 						System.out.println("File location: " + folderPath+File.separator+file.getName());
 					}
 				} else if (file.isDirectory()) {
-					System.out.println("\nFolder Name: " + file.getName());
 					searchFile(fileName, folderPath + "\\" + file.getName());
 				}
 			}
